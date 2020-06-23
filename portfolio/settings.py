bc8af0c9d2b04fc8b787cf1cb0e17c454748d2bf
prime626 <64206222+prime626@users.noreply.github.com>
2020-06-23 18:00:27 +0100

    password change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'abayomi626',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
